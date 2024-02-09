b437bef7937ad5d2f4174e2b86c1d9552f0edeea
vanessabrazuna <brazuna.nessa@gmail.com>
2024-02-09 16:01:11 -0300

    Style: increasing font-size on mobile

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -88,10 +88,10 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3 hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
 
-        <span className="text-sm font-medium text-slate-200">
+        <span className="text-lg md:text-sm font-medium text-slate-200">
           Adicionar nota
         </span>
-        <p className="text-sm leading-6 text-slate-400">
+        <p className="text-lg md:text-sm leading-6 text-slate-400">
           Grave uma nota em áudio que será convertida para texto
           automaticamente.
         </p>
@@ -102,17 +102,17 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
         <Dialog.Content className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
           
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
-            <X className="size-5" />
+            <X className="size-6 md:size-5" />
           </Dialog.Close>
 
           <form className="flex-1 flex flex-col">
             <div className="flex flex-1 flex-col gap-3 p-5">
-              <span className="text-sm font-medium text-slate-300">
+              <span className="text-lg md:text-sm font-medium text-slate-300">
                 Adicionar nota
               </span>
 
               {shoudShowOnBoarding ? (
-                <p className="text-sm leading-6 text-slate-400">
+                <p className="text-lg md:text-sm leading-6 text-slate-400">
                   Comece{" "}
                   <button
                     type="button" 
@@ -133,7 +133,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
               ) : (
                 <textarea
                   autoFocus
-                  className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                  className="text-lg md:text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
                   onChange={handleContentChange}
                   value={content}
                 />
@@ -144,16 +144,16 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
               <button
                 type="button"
                 onClick={handleStopRecording}
-                className="w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100"
+                className="w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-lg md:text-sm text-slate-300 outline-none font-medium hover:text-slate-100"
               >
-                <div className="size-3 rounded-full bg-red-500 animate-pulse" />
+                <div className="size-4 md:size-3 rounded-full bg-red-500 animate-pulse" />
                 Gravando! (clique p/ interromper)
               </button>
             ) : (
               <button
                 type="button"
                 onClick={handleSaveNote}
-                className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+                className="w-full bg-lime-400 py-4 text-center text-lg md:text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
               >
                 Salvar nota
               </button>
